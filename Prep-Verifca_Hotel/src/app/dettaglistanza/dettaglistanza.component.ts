43b5ccc286c9c372308fca45cb47901a4d5f1992
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-04-26 20:29:11 +0000

    prep-veri2

---
+++ Prep-Verifca_Hotel/src/app/dettaglistanza/dettaglistanza.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Booking } from '../booking.model';
+import { Room } from '../rooms.model';
+
+@Component({
+  selector: 'app-dettaglistanza',
+  templateUrl: './dettaglistanza.component.html',
+  styleUrls: ['./dettaglistanza.component.css']
+})
+export class DettaglistanzaComponent implements OnInit {
+
+  constructor() { }
+  @Input() listaPreno :Booking[] = undefined!;
+  ngOnInit(): void {
+  }
+  mostraDati(room:Room){
+    console.log(room);
+  }
+}
